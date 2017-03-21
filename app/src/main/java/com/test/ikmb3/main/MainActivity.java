23ab62c925d7f8c130a4233121c3ca7eaf4f4010
2l8um8 <2l8um8@gmail.com>
2017-03-21 11:11:23 +0100

    repositories

--- app/src/main/java/com/test/ikmb3/main/MainActivity.java
+++ app/src/main/java/com/test/ikmb3/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.test.ikmb3;
+package com.test.ikmb3.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.test.ikmb3.R;
 
 public class MainActivity extends AppCompatActivity {
 
